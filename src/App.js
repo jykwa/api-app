dc87b2c68f85b8101d36e12701dae36da9daa28c
Josh Kwa <joshkwa@Joshs-MBP.attlocal.net>
2019-10-29 00:14:33 -0700

    Made first API call and printed out JSON

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
@@ -19,6 +20,48 @@ function App() {
           Learn React
         </a>
       </header>
+      <Example/>
+    </div>
+  );
+}
+
+// function Example() {
+//   return (
+//     <div>
+//       Example text
+//     </div>
+//   )
+// }
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("dummy text");
+
+
+  const text = axios.get('https://api.pushshift.io/reddit/search/comment/?q=science')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      setData(JSON.stringify(response));
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });;
+
+  console.log(text);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <p>{data}</p>
+      <p>data should be above here</p>
     </div>
   );
 }
