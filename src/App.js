bbbd382ed646259b7d55fa1ad5c4bfd79f3c855a
Josh Kwa <joshkwa@Joshs-MBP.attlocal.net>
2019-10-29 00:59:59 -0700

    Refactored, added a button to show API

--- src/App.js
+++ src/App.js
@@ -36,32 +36,37 @@ function App() {
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
-  const [data, setData] = useState("dummy text");
+  const [data, setData] = useState([]);
 
+  const url = 'https://api.pushshift.io/reddit/search/comment/?q='
+  const searchTerm = 'vgcs';
 
-  const text = axios.get('https://api.pushshift.io/reddit/search/comment/?q=science')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-      setData(JSON.stringify(response));
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });;
+  function callAPI() {
+    axios.get(url + searchTerm)
+      .then(function (response) {
+        // handle success
+        console.log(response.data.data);
+        setData(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
 
-  console.log(text);
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
-      <p>{data}</p>
+      <button onClick={callAPI}>Call the API</button>
+      <div>{data.map(({ body, id }, index) => <p key={index}>{body} {id}</p>)}</div>
       <p>data should be above here</p>
+
     </div>
   );
 }
