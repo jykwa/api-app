ecda5c42b622b0e640036e8a2c6e24c64caecc31
Josh Kwa <joshkwa@Joshs-MBP.attlocal.net>
2020-01-07 17:30:07 -0800

    Removed initial react header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload test test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Example/>
+      <Example />
     </div>
   );
 }
@@ -38,21 +24,22 @@ function Example() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
 
-  const url = 'https://api.pushshift.io/reddit/search/comment/?q='
-  const searchTerm = 'vgcs';
+  const url = "https://api.pushshift.io/reddit/search/comment/?q=";
+  const searchTerm = "vgcs";
 
   function callAPI() {
-    axios.get(url + searchTerm)
-      .then(function (response) {
+    axios
+      .get(url + searchTerm)
+      .then(function(response) {
         // handle success
         console.log(response.data.data);
         setData(response.data.data);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         // handle error
         console.log(error);
       })
-      .finally(function () {
+      .finally(function() {
         // always executed
       });
   }
@@ -60,13 +47,16 @@ function Example() {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
       <button onClick={callAPI}>Call the API</button>
-      <div>{data.map(({ body, id }, index) => <p key={index}>{body} {id}</p>)}</div>
+      <div>
+        {data.map(({ body, id }, index) => (
+          <p key={index}>
+            {body} {id}
+          </p>
+        ))}
+      </div>
       <p>data should be above here</p>
-
     </div>
   );
 }
