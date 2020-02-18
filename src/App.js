025f462394a7b61bbd146337b20af0d42c552499
Josh Kwa <joshkwa@Joshs-MBP.attlocal.net>
2020-02-17 23:58:52 -0800

    Testing out wheretocredit api

--- src/App.js
+++ src/App.js
@@ -19,21 +19,82 @@ function App() {
 //   )
 // }
 
+// This is the API for redditsearch
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+//   const [data, setData] = useState([]);
+
+//   const url = "https://api.pushshift.io/reddit/search/comment/?q=";
+//   const searchTerm = "vgcs";
+
+//   function callAPI() {
+//     axios
+//       .get(url + searchTerm)
+//       .then(function(response) {
+//         // handle success
+//         console.log(response.data.data);
+//         setData(response.data.data);
+//       })
+//       .catch(function(error) {
+//         // handle error
+//         console.log(error);
+//       })
+//       .finally(function() {
+//         // always executed
+//       });
+//   }
+
+// function callAPI() {
+//   axios
+//     .get(url + searchTerm)
+//     .then(function(response) {
+//       // handle success
+//       console.log(response.data.data);
+//       setData(response.data.data);
+//     })
+//     .catch(function(error) {
+//       // handle error
+//       console.log(error);
+//     })
+//     .finally(function() {
+//       // always executed
+//     });
+// }
+
+// This is the API for wheretocredit
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
 
-  const url = "https://api.pushshift.io/reddit/search/comment/?q=";
-  const searchTerm = "vgcs";
+  const url = "https://www.wheretocredit.com/api/2.0/calculate";
+  const requestBody = [
+    {
+      id: "abcdxyz",
+      ticketingCarrier: "UA",
+      baseFareUSD: 483.9,
+      segments: [
+        {
+          origin: "SFO",
+          destination: "HKG",
+          // departure: "2016-02-04T18:57:00.000Z",
+          carrier: "UA",
+          operatingCarrier: "UA",
+          bookingClass: "W"
+          // flightNumber: "472"
+        }
+      ]
+    }
+  ];
 
   function callAPI() {
     axios
-      .get(url + searchTerm)
+      .post(url, requestBody)
       .then(function(response) {
         // handle success
-        console.log(response.data.data);
-        setData(response.data.data);
+        console.log(response.data.value);
+        setData(response.data.value);
       })
       .catch(function(error) {
         // handle error
@@ -44,19 +105,30 @@ function Example() {
       });
   }
 
+  function parseData(data) {
+    return data.map((obj, index) => (
+      <div>
+        {JSON.stringify(obj.value.totals)}
+
+        {obj.value.totals.map(({ id, name, rdm }) => (
+          <p key={index}>
+            {id} {name}{" "}
+            {rdm.map(mile => (
+              <p>{mile}</p>
+            ))}
+          </p>
+        ))}
+      </div>
+    ));
+  }
+
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
       <button onClick={callAPI}>Call the API</button>
-      <div>
-        {data.map(({ body, id }, index) => (
-          <p key={index}>
-            {body} {id}
-          </p>
-        ))}
-      </div>
-      <p>data should be above here</p>
+      <div>{parseData(data)}</div>
+      <p>data should be above here (Data provided by wheretocredit.com)</p>
     </div>
   );
 }
